9ed1ea59d3a59323bebc0dcf321656e5bde43382
andreymlv <ndrmlv@ya.ru>
2023-04-11 19:03:39 +0300

    init commit

---
+++ lib/include/game.h
@@ -0,0 +1,67 @@
+#if !defined(GAME_LIB_GAMEH)
+#define GAME_LIB_GAMEH
+#include <string>
+namespace game {
+constexpr int16_t kHeight = 500;
+constexpr int16_t kWidth = 500;
+
+class Position {
+  int16_t x_;
+  int16_t y_;
+};
+
+class Cell {
+  std::string texture_;
+  bool canPass_;
+};
+
+class World {
+  Cell cells_[kWidth][kHeight];
+};
+
+class Item {
+  int16_t id_;
+};
+
+class Enemy {
+  int16_t health_;
+  float speed_;
+  int16_t drop_;
+};
+
+class Hero {
+  int16_t health_;
+  int16_t arrows_;
+  int16_t bombs_;
+  Item inventory_[10];
+  float speed_;
+};
+
+class Npc {
+  int16_t health_;
+  int16_t drop_;
+  float speed_;
+};
+
+class Cam {
+  Position pos_;
+  int16_t height_;
+  int16_t width_;
+};
+
+class Window {
+	
+};
+
+class Game {
+ private:
+  std::string title_;
+
+ public:
+  Game();
+  void run();
+};
+
+}  // namespace game
+
+#endif  // GAME_LIB_GAMEH
---
+++ lib/src/game.cpp
@@ -0,0 +1,10 @@
+#include "game.h"
+
+game::Game::Game() : title_("Zelda Clone") {}
+
+void game::Game::run() {
+    // Init window
+    // Show game
+    // Other stuff
+    // End
+}
